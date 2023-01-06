b05d8406b4b83a548a59367738168790345b6716
Rafael Brendo <rafalive122@hotmail.com>
2023-01-06 13:43:46 -0300

    findAll implementation.

--- src/application/Program.java
+++ src/application/Program.java
@@ -31,6 +31,13 @@ public class Program {
 		for(Seller obj: list) {
 			System.out.println(obj);
 		}
+		
+		System.out.println("\n=== TEST 3: seller findAll ===");
+		//Como eu já declarei o list, eu posso reaproveita-lo
+		list = sellerDao.findAll();
+		for(Seller obj: list) {
+			System.out.println(obj);
+		}
 
 		
 		
